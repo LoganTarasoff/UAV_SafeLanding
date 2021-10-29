1ca83fbe5fd044b95976ccdb76108da6d8ece207
vishalbalasubramanian <79001789+vishalbalasubramanian@users.noreply.github.com>
2021-10-28 21:47:40 -0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -15,10 +15,12 @@ import imageio
 #Load safe data
 for image_path_safe in glob.glob("UAV_SafeLanding/Dataset/Safe/*.png"):
      image_safe = imageio.imread(image_path_safe)               #loads all safe images
-     image_safe = imgage_safe.resize((64,64))              #resizes all images to 64x64
+     image_safe = image_safe.resize((64,64))                    #resizes all images to 64x64
+     image_safe = image_safe.convert('L')                       #converts all the resized images to grayscale
 
 #Load unsafe data
 for image_path_unsafe in glob.glob("UAV_SafeLanding/Dataset/Unsafe/*.png"):
-     image_unsafe = imageio.imread(image_path_unsafe)              #loads all unsafe images
-     image_unsafe = imgage_unsafe.resize((64,64))                  #resizes all images to 64x64
+     image_unsafe = imageio.imread(image_path_unsafe)           #loads all unsafe images
+     image_unsafe = image_unsafe.resize((64,64))                #resizes all images to 64x64
+     image_unsafe = image_unsafe.convert('L')                   #converts all the resized images to grayscale
 
