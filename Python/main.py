ae9b4fa7bcb042152780558ac90f1c22e3371a29
Alex <92649523+alexmac6@users.noreply.github.com>
2021-10-29 18:35:55 -0700

    Update main.py
    code works now :)

--- Python/main.py
+++ Python/main.py
@@ -1,26 +1,23 @@
-import numpy as np
+#Import Libraries
 
-import os, glob
-
-print("This is a test to see if I can add a file to the repo")
-
-#%% Import Photos, rescale to 64x64, and greyscale them
-
-import os
 import glob
+import numpy as np
 from PIL import Image
-import imageio
-#%% load safe and unsafe images and convert to 64x64
+#%% Load images and convert them to 64x64 and grayscale
 
 #Load safe data
-for image_path_safe in glob.glob("UAV_SafeLanding/Dataset/Safe/*.png"):
-     image_safe = imageio.imread(image_path_safe)               #loads all safe images
+for image_path_safe in glob.glob("C:/Users/alexm/UAV_SafeLanding/Dataset/Safe/*.png"): # Note must use local path for image loading
+     image_safe = Image.open(image_path_safe)                   #loads all safe images
      image_safe = image_safe.resize((64,64))                    #resizes all images to 64x64
      image_safe = image_safe.convert('L')                       #converts all the resized images to grayscale
-
+     #image_safe.save(image_path_safe, .png)                    #saves the images as a .png
 #Load unsafe data
-for image_path_unsafe in glob.glob("UAV_SafeLanding/Dataset/Unsafe/*.png"):
-     image_unsafe = imageio.imread(image_path_unsafe)           #loads all unsafe images
+for image_path_unsafe in glob.glob("C:/Users/alexm/UAV_SafeLanding/Dataset/Safe/*.png"): #Note: Must use local path for image loading!
+     image_unsafe = Image.open(image_path_unsafe)               #loads all unsafe images
      image_unsafe = image_unsafe.resize((64,64))                #resizes all images to 64x64
      image_unsafe = image_unsafe.convert('L')                   #converts all the resized images to grayscale
+     #image_unsafe.save(''.png)                                 #saves the images as a .png
+
+#%% Cross Entropy Cost Function
+
 
